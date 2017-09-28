8a05a6d262934f09c86d26413822b1a61d292020
Dmitry <tsybulkadmitry@gmail.com>
2017-09-28 23:15:04 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this._min = null;
+    	this._max = null;
+    }
 
     setRange(min, max) {
-
+    	this._min = min;
+    	this._max = max;
     }
 
     guess() {
-
+    	let current = Math.round((this._min+this._max)/2);
+    	return current;
     }
 
     lower() {
-
+    	this._max = Math.round((this._min+this._max)/2);
     }
 
     greater() {
-
+    	this._min = Math.round((this._min+this._max)/2);
     }
 }
 
 module.exports = GuessingGame;
+
